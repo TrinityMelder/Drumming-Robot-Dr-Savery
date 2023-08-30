52216f9decf2d2171fce0e4cab2c96d492ee0886
Trinity Melder <34265871+TrinityMelder@users.noreply.github.com>
2023-08-31 09:40:34 +1000

    Update All_Colors.py

--- All_Colors.py
+++ All_Colors.py
@@ -6,29 +6,23 @@ import random
 # LED SETUP
 NUM_LEDS = 96
 BRIGHTNESS = 0.5
-NUM_CYCLES = 5
 
-led_strip = neopixel.NeoPixel(board.D18, NUM_LEDS, brightness=BRIGHTNESS, auto_write=False)  # Use the appropriate pin
+led_strip = neopixel.NeoPixel(board.Data, NUM_LEDS, brightness=BRIGHTNESS, auto_write=True)  # Use the appropriate pin
 
 def random_color_pattern():
     try:
-        for _ in range(NUM_CYCLES):
+        while True:
             for i in range(NUM_LEDS):
                 # Generate a random color
                 random_color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
 
-                # Turn on the current LED with the random color
-                led_strip.fill((0, 0, 0))  # Turn off all LEDs
+                # Set the current LED to the random color
                 led_strip[i] = random_color
-                led_strip.show()
-                time.sleep(1)  # LED on time
 
-                # Turn off the current LED
-                led_strip[i] = (0, 0, 0)  # Turn off the current LED
-                led_strip.show()
-                time.sleep(0.1)  # Delay between LEDs
-            
-            # Delay between cycles
+            # Show the pattern on the LED strip
+            led_strip.show()
+
+            # Delay before generating the next pattern
             time.sleep(1)
 
     except KeyboardInterrupt:

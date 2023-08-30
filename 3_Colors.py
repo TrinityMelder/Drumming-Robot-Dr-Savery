9261eeaf7f8c18b54e0f19aa898bc4f86c289178
Trinity Melder <34265871+TrinityMelder@users.noreply.github.com>
2023-08-31 09:42:08 +1000

    Update 3_Colors.py

--- 3_Colors.py
+++ 3_Colors.py
@@ -7,27 +7,39 @@ import random
 NUM_LEDS = 96
 BRIGHTNESS = 0.5
 
-led_strip = neopixel.NeoPixel(board.D18, NUM_LEDS, brightness=BRIGHTNESS, auto_write=True)  # Use the appropriate pin
+led_strip = neopixel.NeoPixel(board.Data, NUM_LEDS, brightness=BRIGHTNESS, auto_write=True)  # Use the appropriate pin
 
-def limited_color_pattern():
+def color_sequence_pattern():
     try:
-        colors = [(255, 0, 0), (0, 255, 0), (0, 0, 255)]  # Red, Green, Blue
-
         while True:
-            for color in colors:
-                for i in range(NUM_LEDS):
-                    led_strip[i] = color
+            for i in range(NUM_LEDS):
+                red = (255, 0, 0)
+                green = (0, 255, 0)
+                blue = (0, 0, 255)
 
-                # Show the pattern on the LED strip
-                led_strip.show()
+                if i % 3 == 0:
+                    led_strip[i] = red
+                    led_strip[i + 1] = green
+                    led_strip[i + 2] = blue
+                elif i % 3 == 1:
+                    led_strip[i] = green
+                    led_strip[i + 1] = blue
+                    led_strip[i + 2] = red
+                else:
+                    led_strip[i] = blue
+                    led_strip[i + 1] = red
+                    led_strip[i + 2] = green
 
-                # Delay before generating the next pattern
-                time.sleep(1)
+            # Show the pattern on the LED strip
+            led_strip.show()
+
+            # Delay before generating the next pattern
+            time.sleep(1)
 
     except KeyboardInterrupt:
         # If Ctrl+C is pressed, turn off all LEDs and exit the program
         led_strip.fill((0, 0, 0))
         led_strip.show()
 
-# Call the function to run the limited color pattern
-limited_color_pattern()
+# Call the function to run the color sequence pattern
+color_sequence_pattern()
